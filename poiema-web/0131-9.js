1ca9ed581c92c35b5453c4c176e05b654c6f3576
7732-18-5 <anhedonia00@gmail.com>
2018-01-31 12:59:33 +0900

    fds0131|for while 과제 (9/10)

---
+++ poiema-web/0131-9.js
@@ -0,0 +1,27 @@
+// 삼각형출력하기 
+// 다음을 참고하여 * (별)로 높이가 5인(var line = 5) 삼각형을 문자열로 완성하라.
+// 개행문자(‘\n’) 를 사용하여 개행한다.
+// 완성된 문자열의 마지막은 개행문자(‘\n’)로 끝나도 관게없다.
+// 높이(line) : 5
+var asterisk = '';
+var tri = '';
+for (var i = 0; i < 5; i++) {
+    asterisk += '*';
+    tri += asterisk + '\n';
+}
+    console.log(tri);
+// 
+do {
+    console.log('asterisk')
+}
+// 
+var asterisk = '';
+
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+        asterisk += '\n';
+            console.log(\n)
+        }
+    }
+}
+//
--- poiema-web/0131.js
+++ poiema-web/0131.js
@@ -3,11 +3,14 @@ for (var i = 0; i < 10; i++) {
     if(i%2==0)
     console.log(i);
 }
-// for문을 사용하여 0부터 10미만의 정수 중에서 홀수만을 작은 수부터 출력하시오.
+// for문을 사용하여 0부터 10미만의 정수 중에서 홀수만을 작은 수부터 문자열로 출력하시오.
+var num = '';
 for (var i = 0; i < 10; i++) {
-    if (i % 2 == 1)
-        console.log(i);
+    if (i % 2 == 1){
+        num += i;
+    }
 }
+console.log(num);
 // for문을 사용하여 0부터 10미만의 정수 중에서 홀수만을 큰 수부터 출력하시오.
 for (var i = 10; i > 0; i--) {
     if (i % 2 == 1)
@@ -43,3 +46,11 @@ for (var i = 0; i < 10; i++) {
 
 console.log(x);
 
+// 1 ~ 20 의 정수 중, 2 또는 3의 배수가 아닌 수의 총합을 구하시오.
+var x = 0;
+for (var i = 0; i <= 20; i++) {
+    if (i % 3 && i % 2 != 0) {
+        x += i;
+    }
+}
+console.log(x);

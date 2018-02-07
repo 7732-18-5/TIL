a1d01f40c637df5104ae07c93c4a039ad6f5d164
7732-18-5 <anhedonia00@gmail.com>
2018-02-07 13:00:16 +0900

    fds0207|Iterate

---
+++ web3javascript/fds0207-1.js
@@ -0,0 +1,16 @@
+var s = 1;
+
+for(var n = 1; n <= 5; n++){
+    s += 2*n
+}
+console.log(s)
+
+var t = 1;
+var n = 1;
+while(n<5){
+    t += 2*n;
+    n++
+}
+console.log(t)
+
+console.log(s-t)
---
+++ web3javascript/fds0207-2.js
@@ -0,0 +1,7 @@
+var s = 20;
+var n = 1;
+for(s = 20; s <= 0; s--){
+    s+=(20-(3*n))
+    n++
+}
+console.log(s=Math.abs(s)+n)

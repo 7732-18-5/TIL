d78fe1fc42e64949f64446ed3ad7e98038c3b7f9
7732-18-5 <anhedonia00@gmail.com>
2018-02-08 18:00:47 +0900

    fds0208|생활코딩 function

---
+++ web3javascript/fds0208-function.js
@@ -0,0 +1,20 @@
+function numbering() {
+    document.write(1);
+}
+// 변수 선언, 변수 사용방법
+var a = 1;
+alert(a);
+// 함수 호출 방법
+numbering();
+// numbering 뒤에 ()가 없다면 함수가아닌  변수라고 인식 할 것이다.
+numbering;
+
+//
+function numbring() {
+    i = 0 ;
+    while(i < 10) {
+        document.write(i);
+        i += 1;
+    }
+}
+numbring();

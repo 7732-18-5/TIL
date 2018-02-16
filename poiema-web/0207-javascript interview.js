35d3711066456359a84a6bc668150288dfd460ad
7732-18-5 <anhedonia00@gmail.com>
2018-02-17 00:22:48 +0900

    fds0217|0206 javascript 오답노트(1,4,9)

---
+++ poiema-web/0207-javascript interview.js
@@ -0,0 +1,15 @@
+// 1. index.html 에서 main.js 를 로드하기 위한 코드를 완성하라 단 index.html 은 프로젝트 루트에 위치하고 main.js 는 src / js 디렉터리에 위치한다.
+<html>
+<head>
+</head>
+<body>
+    <script src="src/js/main.js"> </script>
+</body>
+</html>
+
+// 4. var foo = 42 / -0 console.log(foo); 의 결과를 기술
+-infinity
+
+// 9. console.log(!!null); 의 결과는 무엇인가?
+!! 는 피연산자를 boolean으로 변환한다.
+즉 답은 'false' 이다.
